26b074659d2ec573dfab5f65dab14468344795c5
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-11-02 11:53:48 +0100

    search component add dynamic code to html

--- ecommerce-front/src/app/services/product.service.ts
+++ ecommerce-front/src/app/services/product.service.ts
@@ -3,13 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { Product } from '../common/product';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  
 
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-categories';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -20,6 +23,18 @@ export class ProductService {
       map(response => response.content)  // Extract the products array from `content`
     );
   }
+
+
+  getProductCategories(): Observable<ProductCategory[]> {
+
+    return this.httpClient.get<ProductCategory[]>(this.categoryUrl);
+  }
+
+  /**
+   * console.log('getProductList() called');
+    
+    return this.httpClient.get<Product[]>(this.baseUrl);
+   */
   
 
 
@@ -30,4 +45,10 @@ interface GetResponseProducts {
   totalPages: number;       // Total number of pages
   size: number;             // Page size
   number: number;           // Current page number
+}
+
+interface GetResponseProductCategory {
+
+    productCategory: ProductCategory[];
+  
 }
