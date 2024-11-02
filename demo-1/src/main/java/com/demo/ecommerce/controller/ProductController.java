c234c338944a47014697be7278ea6e1078ce1b54
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-11-02 09:59:43 +0100

    change URL to make it more sence

--- demo-1/src/main/java/com/demo/ecommerce/controller/ProductController.java
+++ demo-1/src/main/java/com/demo/ecommerce/controller/ProductController.java
@@ -55,7 +55,7 @@ public class ProductController {
     }
     
 
-    @GetMapping("/category/{id}")
+    @GetMapping("/products/search/findByCategoryId/{id}")
     public Page<Product> getProductsByCategoryId(
             @PathVariable("id") Long id,
             Pageable pageable) {
