de61387483d5d9c13c473caf5c1138f26fc599b6
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-11-03 18:34:48 +0100

    Angular - Define form in component.ts ,layout from controls in HTML template and add event handler for form submisson

--- ecommerce-front/src/app/components/checkout/checkout.component.ts
+++ ecommerce-front/src/app/components/checkout/checkout.component.ts
@@ -1,10 +1,82 @@
 import { Component } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css'
+  styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent {
 
+  checkoutFormGroup = new FormGroup({
+    customer: new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email])
+    }),
+    shippingAddress: new FormGroup({
+      street: new FormControl('', Validators.required),
+      city: new FormControl('', Validators.required),
+      state: new FormControl('', Validators.required),
+      country: new FormControl('', Validators.required),
+      zipCode: new FormControl('', Validators.required)
+    }),
+    billingAddress: new FormGroup({
+      street: new FormControl('', Validators.required),
+      city: new FormControl('', Validators.required),
+      state: new FormControl('', Validators.required),
+      country: new FormControl('', Validators.required),
+      zipCode: new FormControl('', Validators.required)
+    }),
+    creditCard: new FormGroup({
+      cardType: new FormControl('', Validators.required),
+      nameOnCard: new FormControl('', Validators.required),
+      cardNumber: new FormControl('', [Validators.required, Validators.pattern(/^\d{16}$/)]),
+      securityCode: new FormControl('', [Validators.required, Validators.pattern(/^\d{3}$/)]),
+      expirationMonth: new FormControl('', Validators.required),
+      expirationYear: new FormControl('', Validators.required)
+    })
+  });
+
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  copyShippingAddressToBillingAddress(event: Event) {
+    const target = event.target as HTMLInputElement;
+    console.log("Checkbox changed:", target.checked);
+
+    if (target.checked) {
+      const shippingAddressValue = this.checkoutFormGroup.get('shippingAddress')?.value || {
+        street: '',
+        city: '',
+        state: '',
+        country: '',
+        zipCode: ''
+      };
+      console.log("Copying shipping address to billing address:", shippingAddressValue);
+      this.checkoutFormGroup.get('billingAddress')?.setValue(shippingAddressValue);
+    } else {
+      console.log("Resetting billing address");
+      this.checkoutFormGroup.get('billingAddress')?.reset();
+    }
+  }
+
+  onSubmit() {
+    if (this.checkoutFormGroup.invalid) {
+      console.log("Invalid form");
+    } else {
+      console.log("Handling the submit button");
+      console.log("Customer Information:", this.checkoutFormGroup.get('customer')?.value);
+      console.log("Shipping Address:", this.checkoutFormGroup.get('shippingAddress')?.value);
+      console.log("Billing Address:", this.checkoutFormGroup.get('billingAddress')?.value);
+      console.log("Credit Card Details:", this.checkoutFormGroup.get('creditCard')?.value);
+      console.log("The email address is " + this.checkoutFormGroup.get('customer')?.value.email);
+    }
+  }
 }
