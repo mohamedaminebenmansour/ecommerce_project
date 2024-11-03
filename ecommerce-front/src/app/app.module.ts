b452c36e9911cf331f2243697c71cf842a047fdf
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-11-03 10:06:57 +0100

    Angular - Add new route for CCartDetailsComponent

--- ecommerce-front/src/app/app.module.ts
+++ ecommerce-front/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 const routes: Routes = [
+  {path: 'cart-details', component: CartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
   {path: 'search/:keyword', component: ProductListComponent},
   {path: 'category/:id', component: ProductListComponent},
