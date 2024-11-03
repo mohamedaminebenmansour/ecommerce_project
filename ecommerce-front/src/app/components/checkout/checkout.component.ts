6ce5f11d83ab97cd472a6c6d1db57317304bbfc8
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-11-04 00:01:44 +0100

    Angular - Spicify validation rules for the form controls and define Getter methods to access from controls

--- ecommerce-front/src/app/components/checkout/checkout.component.ts
+++ ecommerce-front/src/app/components/checkout/checkout.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  FormArray,
   FormControl,
   FormGroup,
   Validators,
-  ReactiveFormsModule,
 } from '@angular/forms';
 import { CheckoutFormService } from '../../services/checkout-form.service';
 import { Country } from '../../common/country';
@@ -57,6 +55,27 @@ export class CheckoutComponent implements OnInit {
 
     
   }
+
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() { return this.checkoutFormGroup.get('customer.email'); }
+
+  get shippingAddressStreet() { return this.checkoutFormGroup.get('shippingAddress.street'); }
+  get shippingAddressCity() { return this.checkoutFormGroup.get('shippingAddress.city'); }
+  get shippingAddressState() { return this.checkoutFormGroup.get('shippingAddress.state'); }
+  get shippingAddressZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode'); }
+  get shippingAddressCountry() { return this.checkoutFormGroup.get('shippingAddress.country'); }
+
+  get billingAddressStreet() { return this.checkoutFormGroup.get('billingAddress.street'); }
+  get billingAddressCity() { return this.checkoutFormGroup.get('billingAddress.city'); }
+  get billingAddressState() { return this.checkoutFormGroup.get('billingAddress.state'); }
+  get billingAddressZipCode() { return this.checkoutFormGroup.get('billingAddress.zipCode'); }
+  get billingAddressCountry() { return this.checkoutFormGroup.get('billingAddress.country'); }
+
+  get creditCardType() { return this.checkoutFormGroup.get('creditCard.cardType'); }
+  get creditCardNameOnCard() { return this.checkoutFormGroup.get('creditCard.nameOnCard'); }
+  get creditCardNumber() { return this.checkoutFormGroup.get('creditCard.cardNumber'); }
+  get creditCardSecurityCode() { return this.checkoutFormGroup.get('creditCard.securityCode'); }
 
   checkoutFormGroup = new FormGroup({
     customer: new FormGroup({
