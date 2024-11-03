615b956359255ec10239e63513a45c801532bff0
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-11-04 00:33:59 +0100

    Angular - Create FormFieldValidators as exemple for notOnlyWhitespace

--- ecommerce-front/src/app/validators/form-field-validators.ts
+++ ecommerce-front/src/app/validators/form-field-validators.ts
@@ -1,2 +1,17 @@
+import { FormControl, ValidationErrors } from "@angular/forms";
+
 export class FormFieldValidators {
+    static notOnlyWhitespace(control: FormControl) : ValidationErrors {
+        
+        // check if string only contains whitespace
+        if ((control.value != null) && (control.value.trim().length === 0)) {
+
+            // invalid, return error object
+            return { 'notOnlyWhitespace': true };
+        }
+        else {
+            // valid, return null
+            return { 'notOnlyWhitespace': false };
+        }
+    }
 }
