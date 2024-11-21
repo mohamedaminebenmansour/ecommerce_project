b60e5aa4e4d79a883680f35b30765c65d3d3e16b
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-11-21 10:31:41 +0100

    Spring boot - update Customer (associate Authority entity with Customer entity )

--- demo-1/src/main/java/com/demo/ecommerce/entity/Customer.java
+++ demo-1/src/main/java/com/demo/ecommerce/entity/Customer.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -52,6 +53,10 @@ public class Customer {
     @Column(name = "create_dt")
     @JsonIgnore
     private Date createDt;
+    
+    @OneToMany(mappedBy = "customer", fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Set<Authority> authorities;
 
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
     private Set<Order> orders = new HashSet<>();
