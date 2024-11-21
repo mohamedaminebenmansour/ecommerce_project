7c64472671e22adfa308c14d28b85014074b850d
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-11-21 12:50:48 +0100

    Spring boot - Add DTOs for login request and response

---
+++ demo-1/src/main/java/com/demo/ecommerce/dto/LoginRequestDTO.java
@@ -0,0 +1,5 @@
+package com.demo.ecommerce.dto;
+
+public record LoginRequestDTO(String username, String password) {
+}
+
---
+++ demo-1/src/main/java/com/demo/ecommerce/dto/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package com.demo.ecommerce.dto;
+
+public record LoginResponseDTO(String status, String jwtToken) {
+}
